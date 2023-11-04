a5170567089e99d68f91ef6f2ceeab7d33a622e2
asbest5 <121740041+asbest5@users.noreply.github.com>
2023-11-05 07:16:18 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showNumber(8)
+    basic.pause(5000)
+})
+input.onGesture(Gesture.SixG, function () {
+    basic.showNumber(6)
+    basic.pause(5000)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showNumber(1)
+    basic.pause(5000)
+})
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showNumber(3)
+    basic.pause(5000)
+})
